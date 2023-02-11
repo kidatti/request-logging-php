a24a7294ae5274c78804a36286bef86235f67a04
kidatti <kidatti@gmail.com>
2023-02-12 01:00:23 +0900

    CORSをすべて許可、ログフォルダを作成、POSTおよびFILEへ対応

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+$dir_log = 'logs/' . date("Ymd-his") . '/';
+mkdir($dir_log);
 $request = [
     'method' => $_SERVER["REQUEST_METHOD"],
     'url' => $_SERVER['REQUEST_URI'],
@@ -14,5 +17,14 @@ foreach (getallheaders() as $name => $value) {
 foreach ($_GET as $key => $value) {
     $request['get'][$key] = $value;
 }
-print_r($request,true);
-file_put_contents('log.txt', print_r($request,true));
+// post
+foreach ($_POST as $key => $value) {
+    $request['post'][$key] = $value;
+}
+// post
+foreach ($_FILES as $key => $value) {
+    $request['files'][$key] = $value;
+    print_r($value);
+    move_uploaded_file($value['tmp_name'], $dir_log . $value['name']);
+}
+file_put_contents($dir_log . 'log.txt', print_r($request,true));
